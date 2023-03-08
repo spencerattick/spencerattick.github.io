0d874bd32a4c706e4f4e8c51235af9b7a2c0efc2
Spencer Attick <spencer.attick@gmail.com>
2023-03-08 10:02:29 -0500

    update server to point at dist/

--- server/server.js
+++ server/server.js
@@ -2,37 +2,38 @@ const { resolveNs } = require('dns');
 const express = require('express');
 const app = express();
 const { parse } = require('rss-to-json');
+const path = require('path');
 
 // Allow cross-origin requests from Vite
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     next();
-  });
+});
+
+// Serve the files in the dist directory
+app.use(express.static(path.join(__dirname, 'dist')));
 
 let blogRss;
 let goodReadsRss;
 
-
 (async () => {
-  blogRss = await parse('https://medium.com/feed/@spencer.attick');
+    blogRss = await parse('https://medium.com/feed/@spencer.attick');
 })();
 
 (async () => {
-  goodReadsRss = await parse('https://www.goodreads.com/user/updates_rss/104822881');
-
+    goodReadsRss = await parse('https://www.goodreads.com/user/updates_rss/104822881');
     console.log(goodReadsRss)
 })();
 
-
 // The middleware should be above your routes
 app.get('/api/posts', (req, res) => {
-  res.json(blogRss.items);
+    res.json(blogRss.items);
 });
 
 app.get('/api/feed', (req, res) => {
-  res.json(goodReadsRss);
+    res.json(goodReadsRss);
 });
 
 app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+    console.log('Server started on http://localhost:3000');
 });
